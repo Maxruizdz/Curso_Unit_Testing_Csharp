5741a0674050b736c4878245d02faf7232393738
Maxruizdz <maxruizdz@hotmail.com>
2024-01-29 09:35:55 -0300

    Atributos de configuracion en Coverlet

--- StringManipulation/IFileReaderConector.cs
+++ StringManipulation/IFileReaderConector.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace StringManipulation
 {
+    [ExcludeFromCodeCoverage]
     public class FileReaderConector : IFileReaderConector
     {
         public string ReadString(string fileName)
